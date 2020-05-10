ec6991172ea0480353cbd3cfdea6561df96cf79f
psabela@hotmail.com <psabela@hotmail.com>
2020-05-10 16:10:53 -0400

    updated data

--- fairfaxcountycoronavirus.R
+++ fairfaxcountycoronavirus.R
@@ -38,7 +38,7 @@ df %>% ggplot() +
   theme(panel.grid.minor =   element_blank(),
         panel.grid.major =   element_line(colour = "white",size=0.75),
         axis.text.x = element_text(angle = 90, hjust = 1)) +
-  scale_y_continuous(position = "right", breaks = seq(from = 0,to = 5000,by = 500)) +
+  scale_y_continuous(position = "right", breaks = seq(from = 0,to = 7000,by = 500)) +
   geom_col(aes(x = date, y = deltas))
 
 
